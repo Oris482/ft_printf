b6edc61df7836acb6388aadcf0865c1c8e875e66
jaesjeon <jaesjeon@student.42seoul.kr>
2022-01-21 19:26:17 +0900

    making func_table_bonus

--- src_b/ft_printf_bonus.c
+++ src_b/ft_printf_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jaesjeon <jaesjeon@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 16:56:10 by jaesjeon          #+#    #+#             */
-/*   Updated: 2022/01/20 21:54:47 by jaesjeon         ###   ########.fr       */
+/*   Updated: 2022/01/21 16:06:51 by jaesjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	ft_printf(const char *str, ...)
 	var_p = NULL;
 	while (*str)
 	{
-		tmp = func_table(&status[0], *str, &var_p, ap);
+		tmp = func_table(&status[0], str, &var_p, ap);
 		if (tmp >= 0)
 			cnt += tmp;
 		else
